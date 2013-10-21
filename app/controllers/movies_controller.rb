09bd5cf92e31ffa2915e1ad4c4030f229cb0a35f
Trevork23 <Trevork23@live.com>
2013-10-21 07:12:51 -0700

    HW2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,40 @@
 class MoviesController < ApplicationController
 
   def show
-    id = params[:id] # retrieve movie ID from URI route test test test
+    id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
-    @movies = Movie.all
+    sort = params[:sort]
+    @sort = sort
+    if (params[:sort])
+      session[:sort] = sort
+    end
+
+    @filter_ratings = []
+    @filter_ratings_hash = {}
+    if (params[:ratings])# || session[:ratings])
+       #if (!params[:ratings])
+       #  params[:ratings] = session[:ratings]
+       #end
+
+       params[:ratings].each_key { |keyr| 
+         @filter_ratings.push(keyr)
+         @filter_ratings_hash[keyr] = keyr 
+       }
+       @movies = Movie.find(:all, :order=>sort, :conditions => {:rating => @filter_ratings})
+       session[:ratings] = @filter_ratings_hash
+    else
+       if (session[:ratings])
+         flash.keep
+         redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
+       end
+       @movies = Movie.all(:order=>sort)
+    end
+
+    @all_ratings = ['G','PG','PG-13','R']
   end
 
   def new
